3437c30a3ef983eb0f35fc9142a8a3f119175dd7
Hi-Im-Yuri <tavernajustin@gmail.com>
2026-06-16 22:33:40 -0400

    added single interaction with gemini through cli

--- main.py
+++ main.py
@@ -1,6 +1,31 @@
+import os, argparse
+from dotenv import load_dotenv
+from google import genai
+from google.genai import types
+
+load_dotenv()
+api_key = os.environ.get("GEMINI_API_KEY")
+if api_key == None:
+    raise RuntimeError("no api key found")
+client = genai.Client(api_key=api_key)
+
+
 def main():
-    print("Hello from ai-agent!")
+    parser = argparse.ArgumentParser(description="Chatbot")
+    parser.add_argument("user_prompt", type=str, help="User prompt")
+    args = parser.parse_args()
+    messages: list[types.Content] = [
+        types.Content(role="user", parts=[types.Part(text=args.user_prompt)])
+    ]
+    response = client.models.generate_content(
+        model="gemini-2.5-flash", contents=messages
+    )
 
+    if not response.usage_metadata:
+       pass
+    else:
+        print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+        print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+    print(response.text)
 
-if __name__ == "__main__":
-    main()
+main()
